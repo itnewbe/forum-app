884d75d6986f1c417e8048bd5d89288f44f3d463
itnewbe <itjakarta00@gmail.com>
2025-07-11 17:04:27 +0700

    Force test to fail

--- cypress/e2e/login.spec.js
+++ cypress/e2e/login.spec.js
@@ -3,7 +3,7 @@
 describe("Login Flow", () => {
   it("should login successfully", () => {
     cy.visit("http://localhost:3000/login");
-    cy.get('input[placeholder="Enter your email"]').type("bella@gmail.com");
+    cy.get('input[placeholder="Enter your email"]').type("bell@gmail.com");
     cy.get('input[placeholder="Enter your password"]').type("123456");
     cy.get("button").contains("Login").click();
     cy.url().should("eq", "http://localhost:3000/");
